482d243e9ea05fb2e48d4512ab30915ca1e68209
henrybaah <henrybaah2016@gmail.com>
2018-05-25 18:40:30 +0000

    done with the navbar and the app intro, carousel

---
+++ sarpmedic/sarpmedic_web/js/main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by henry on 5/12/2018.
+ */
+
+// SideNav Button Initialization
+$(".button-collapse").sideNav();
+// SideNav Scrollbar Initialization
+var sideNavScrollbar = document.querySelector('.custom-scrollbar');
+Ps.initialize(sideNavScrollbar);
+
